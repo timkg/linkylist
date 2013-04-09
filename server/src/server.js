324ccd0120743e8797476dd22afc1e437ec4cb58
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-04-09 17:45:24 +0100

    express, socket.io

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var express = require('express');
 
 var db = require('./db');
 var twitter_search = require('./twitter-search.js');
@@ -13,20 +14,43 @@ exports.start = function() {
 	// mongo db should be initialized once for the application, not for each request
 	// https://groups.google.com/forum/#!msg/node-mongodb-native/mSGnnuG8C1o/Hiaqvdu1bWoJ
 	db.connect(function() {
-		var server = http.createServer().listen(HTTP_PORT, function() {
-			console.log('server started');
+		
+		var app = express();
+		var server = http.createServer(app);
+		var io = require('socket.io').listen(server);
+		io.configure(function () { 
+			io.set("transports", ["xhr-polling"]); 
+			io.set("polling duration", 10); 
 		});
 
-		server.on('request', function(request, response) {
+		server.listen(HTTP_PORT);
+
+		app.get('/', function(request, response) {
 			console.log('request received');
-
-			db.getRecentLinks(function(links){
-
-				response.end(JSON.stringify(links), 'utf8');
-
-			});
-
-			
+			// db.getRecentLinks(function(links){
+			// 	response.end(JSON.stringify(links), 'utf8');
+			// });
+			response.end('index');
 		});
+
+		app.get('/fetch', function(request, response) {
+			console.log('request received');
+			// db.getRecentLinks(function(links){
+			// 	response.end(JSON.stringify(links), 'utf8');
+			// });
+			response.end('fetch');
+		});
+
+		app.get('/socket', function(request, response) {
+			console.log('request received');
+			response.sendfile('client/test/socket.html');
+		});
+
+		io.sockets.on('connection', function (socket) {
+			db.getRecentLinks(function(links){
+				socket.emit('links', {items: links});
+			});
+		});
+
 	});
 };	
