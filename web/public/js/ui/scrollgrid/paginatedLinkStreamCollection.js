5c80fcebc8a0e093686e83712f0ea92dfd047a6c
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-02 12:25:39 +0100

    started paginated stream

---
+++ web/public/js/ui/scrollgrid/paginatedLinkStreamCollection.js
@@ -0,0 +1,17 @@
+(function () {
+	/**/
+	"use strict";
+
+	define([
+
+	], function() {
+
+		// starts request to server to search for links with certain keyword
+		// makes paginated requests - not by index, but "next"
+		// server responds with json collection of links, some with preview, others without
+		//  - depends on what we already have in DB
+		// models handle loading of missing preview data
+
+	});
+
+} ());
