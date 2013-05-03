fbf835963733f355f9071e395fcfb9cd06459f53
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-03 09:55:24 +0100

    formatting, return compiled model right away

--- web/src/models/Embedly.js
+++ web/src/models/Embedly.js
@@ -1,17 +1,19 @@
+var mongoose = require('mongoose');
+
 exports.initEmbedlyModel = function () {
 
 	var embedlyOembedFormat = {
-		"provider_url":String,
-		"description":String,
-		"title":String,
-		"url":String,
-		"thumbnail_width":Number,
-		"thumbnail_url":String,
-		"version":String,
-		"provider_name":String,
-		"type":String,
-		"thumbnail_height":Number
+		"provider_url": String,
+		"description": String,
+		"title": String,
+		"url": String,
+		"thumbnail_width": Number,
+		"thumbnail_url": String,
+		"version": String,
+		"provider_name": String,
+		"type": String,
+		"thumbnail_height": Number
 	};
 
-	this.Embedly = mongoose.model('Embedly', mongoose.Schema(embedlyOembedFormat));
+	return mongoose.model('Embedly', mongoose.Schema(embedlyOembedFormat));
 };
