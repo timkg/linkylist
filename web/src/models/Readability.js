57ce7896986aaad15f402c1496bcb6ffb70c3215
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-03 09:53:43 +0100

    created proper schema for mongoose Readability model

--- web/src/models/Readability.js
+++ web/src/models/Readability.js
@@ -1,8 +1,31 @@
-exports.initReadabilityModel = function () {
+(function() {
+	/**/
+	"use strict";
 
-	var readabilityFormat = {
+	var mongoose = require('mongoose');
 
+	exports.initReadabilityModel = function () {
+
+		var readabilityFormat = {
+			"content": String,
+			"domain": String,
+			"author": String,
+			"url": String,
+			"short_url": String,
+			"title": String,
+			"excerpt": String,
+			"direction": String,
+			"word_count": Number,
+			"total_pages": Number,
+			"date_published": Date,
+			"dek": String,
+			"lead_image_url": String,
+			"next_page_id": Number,
+			"rendered_pages": Number
+		};
+
+		return mongoose.model('Readability', mongoose.Schema(readabilityFormat));
 	};
 
-	this.Readability = mongoose.model('Readability', mongoose.Schema(readabilityFormat));
-};
+
+} ());
