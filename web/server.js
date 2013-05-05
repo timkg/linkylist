9af42da9ecd18db4196a877faa5a812651807824
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-05 12:58:33 +0100

    minor cleanup

--- web/server.js
+++ web/server.js
@@ -5,13 +5,10 @@
 	var http = require('http');
 
 	var db = require('./src/db');
-	var twitter = require('./src/services/twitter');
 	var config = require('./config');
 
 	exports.start = function() {
 
-		// mongo db is initialized once for the application, not for each request
-		// https://groups.google.com/forum/#!msg/node-mongodb-native/mSGnnuG8C1o/Hiaqvdu1bWoJ
 		db.connect(function() {
 
 			console.log(config.app_url);
