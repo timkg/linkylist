ffb877e2f2237baf948eefe5796dbd8c7060459f
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-05 15:35:20 +0100

    using twitterhelper to build the url

--- web/src/services/twitter.js
+++ web/src/services/twitter.js
@@ -3,19 +3,10 @@
 	"use strict";
 
 	var baserequest = require('./baserequest');
+	var twitterhelper = require('../utils/twitterhelper');
 
-	// API version = 1.0
-	var TWITTER_SEARCH_BASE_URL = 'http://search.twitter.com/search.json';
-	var TWITTER_SEARCH_PARAMS = '&filter:links&include_entities=1';
-
-	exports.searchTweetsWithUrlsAbout = function(searchTerm, callback, next_page) {
-		var url = TWITTER_SEARCH_BASE_URL;
-		if( !next_page ) {
-			url += '?q=' + searchTerm + TWITTER_SEARCH_PARAMS;
-		} else {
-			url += next_page;
-		}
-
+	exports.searchTweetsWithUrlsAbout = function(query, callback, next_page) {
+		var url = twitterhelper.buildSearchUrlFromQuery(query);
 		baserequest.httpget(url, function(response) {
 			if (typeof response === 'string') { response = JSON.parse(response); }
 			callback(response);
