129bb223141fe96055afd76f43e8a6f2049745fd
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-05 11:08:44 +0100

    moved helper method into own file

--- web/src/models/Tweet.js
+++ web/src/models/Tweet.js
@@ -73,30 +73,6 @@
 				});
 		};
 
-		TweetModel.extractUrlsFromTweets = function(data) {
-			if( typeof data === 'string' ) { data = JSON.parse(data); }
-			var urls = [];
-			var tweetsWithUrls = [];
-			for( var i = 0, len = data.length; i < len; i++ ) {
-				if( data[i].entities && data[i].entities.urls ) {
-					tweetsWithUrls.push(data[i]);
-					var arrayOfUrlsInTweet = data[i].entities.urls;
-					for( var j = 0, urlsLen = arrayOfUrlsInTweet.length; j < urlsLen; j++ ) {
-						urls.push(arrayOfUrlsInTweet[j].expanded_url);
-					}
-				}
-			}
-
-			var uniqueUrls = [];
-			urls.map(function(url) {
-				if (uniqueUrls.indexOf(url) === -1) {
-					uniqueUrls.push(url);
-				}
-			});
-
-			return uniqueUrls;
-		};
-
 		return mongoose.models.TweetModel = TweetModel;
 	};
 
