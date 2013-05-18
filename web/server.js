e760b5c4c4df527ffd9aee1a0129292dbcd1590e
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-18 17:33:06 +0100

    added connect-flash

--- web/server.js
+++ web/server.js
@@ -4,6 +4,7 @@
 	var connect = require('connect');
 	var express = require('express');
 	var http = require('http');
+	var flashMessages = require('connect-flash');
 
 	var auth = require('./src/auth');
 	var db = require('./src/db');
@@ -13,34 +14,43 @@
 
 		db.connect(function() {
 
+			// express app configuration
+			// -------------------------
 			var app = express();
 			app.use(connect.bodyParser());
 			app.use(connect.cookieParser());
 			app.use(connect.session({secret: process.env.APP_SECRET}));
+			app.use(express.compress());
+			app.use(flashMessages());
+
 			// user authentication
 			// -------------------
 			auth.start(app);
 
-			// gzip compression
-			// ----------------
-			app.use(express.compress());
-
+			// view variables
+			// --------------
+			app.use(function(req, res, next) {
+				res.locals.err = req.flash('error');
+				next();
+			});
 
 			// static asset serving
 			// --------------------
-			// "/public" is not part of the HHTP path. /public/js/somefile.js is requested as /js/somefile.js
 			app.use(express.static(__dirname + '/public'));
-			// except for main.js - serve custom main.js for dev vs prod. This file is requested as /public/js/main.js
 			app.get('/public/js/main.js', function(request, response) {
 				response.sendfile( __dirname + '/public/js/main-dev.js');
 			});
 
+			// start http server
+			// -----------------
 			var server = http.createServer(app);
 			server.listen(config.http_port);
 			console.log('server running on port ', config.http_port);
 
 //			var socketio = require('./src/socketio').init(server);
 
+			// start routes
+			// ------------
 			require('./src/routes/main.js').start(app);
 
 		});
