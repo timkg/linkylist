5617a001fb25146878156cae697a507006f80284
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-23 17:38:21 +0100

    added isUrl utility function

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -36,6 +36,10 @@
 				});
 			});
 		};
+		App.isUrl = function(s) {
+			var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+			return regexp.test(s);
+		};
 
 		// set common dependencies
 		// -----------------------
