6ccc17d2ba13e49505f8129982203fdbc1123515
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-05-23 01:24:05 +0100

    restructured due to circular dependency issues

--- web/src/models/EmbedlyExtract.js
+++ web/src/models/EmbedlyExtract.js
@@ -3,14 +3,13 @@
 	"use strict";
 
 	var mongoose = require('mongoose');
-	var LinkModel = require('./Link').compileModel();
+	var LinkModel;
 	var Q = require('q');
 	var array_helper = require('../utils/arrayhelper');
 
 	var embedlyservice = require('../services/embedly');
 
 	exports.compileModel = function() {
-
 		if (mongoose.models.EmbedlyExtractModel) { return mongoose.models.EmbedlyExtractModel; }
 
 		var extractFormat = {
@@ -32,90 +31,92 @@
 
 		var EmbedlyExtractModel = mongoose.model('EmbedlyExtract', mongoose.Schema(extractFormat));
 
-		EmbedlyExtractModel.assignEmbedToLink = function(embed, callback) {
-			LinkModel.findOrCreate({ url: embed.original_url}, function(err, link) {
-				if (err) { throw err; }
-				if (!link._embedlyExtract) {
-					link._embedlyExtract = embed._id;
-					link.save(function(err) {
-						if (err) { callback(new Error(err), null); }
-						callback(null, embed);
-					});
-				} else { callback(null, embed); }
-			});
-		};
-
-		EmbedlyExtractModel.promiseToSaveDocument = function(json) {
-			if (typeof json === 'string') { json = JSON.parse(json); }
-
-			var deferred = Q.defer();
-
-			if (!json.original_url) { deferred.reject(new Error('url not existent or not readable')); }
-
-			EmbedlyExtractModel
-				.findOne({original_url: json.original_url})
-				.exec(function(err, embed) {
-					if (!embed) {
-						EmbedlyExtractModel.create(json, function(err, embed) {
-							if (err || !embed) { deferred.reject(new Error(err)); return false; }
-							EmbedlyExtractModel.assignEmbedToLink(embed, function(err, embed) {
-								if (err || !embed) { deferred.reject(new Error(err)); }
-								deferred.resolve(embed);
-							});
-						});
-					} else {
-						EmbedlyExtractModel.assignEmbedToLink(embed, function(err, embed) {
-							if (err || !embed) { deferred.reject(new Error(err)); }
-							deferred.resolve(embed);
-						});
-					}
-				});
-
-			return deferred.promise;
-		};
-
-		EmbedlyExtractModel.saveDocuments = function(json, callback) {
-			if (typeof json === 'string') { json = JSON.parse(json); }
-			if (!Array.isArray(json)) { json = [json]; }
-
-			var allOperationsAsPromises = [];
-			json.forEach(function(embedValue) {
-				var embedPromise = EmbedlyExtractModel.promiseToSaveDocument(embedValue).fail(function(error) {
-					console.warn(error);
-				});
-				allOperationsAsPromises.push(embedPromise);
-			});
-
-			Q.allResolved(allOperationsAsPromises)
-				.then(function(promises) {
-					var embeds = [];
-					promises.forEach(function(promise) {
-						if (promise.isFulfilled()) {
-							embeds.push(promise.valueOf());
-						} else {
-							var exception = promise.valueOf().exception;
-						}
-					});
-					callback(embeds);
-				})
-				.fail(function(err) {
-					console.log(err);
-				});
-		};
-
 		EmbedlyExtractModel.getExtractForUrls = function(listOfUrls, callback) {
 			if (!Array.isArray(listOfUrls) || (listOfUrls.length === 0 )) {
 				throw new TypeError("EmbedlyExtractModel.getExtractForUrls expects array as first argument");
 			}
 			listOfUrls = array_helper.unique(listOfUrls);
 			embedlyservice.getExtractForUrls(listOfUrls, function(embedlyApiResponse) {
-				EmbedlyExtractModel.saveDocuments(embedlyApiResponse, callback);
+				exports.saveDocuments(embedlyApiResponse, callback);
 			});
 		};
 
 		mongoose.models.EmbedlyExtractModel = EmbedlyExtractModel;
 		return EmbedlyExtractModel;
 	};
+
+	exports.assignEmbedToLink = function(embed, callback) {
+		LinkModel = require('./Link').compileModel();
+		LinkModel.findOrCreate({ url: embed.original_url}, function(err, link) {
+			if (err) { throw err; }
+			if (!link._embedlyExtract) {
+				link._embedlyExtract = embed._id;
+				link.save(function(err) {
+					if (err) { callback(new Error(err), null); }
+					callback(null, embed);
+				});
+			} else { callback(null, embed); }
+		});
+	};
+
+	exports.promiseToSaveDocument = function(json) {
+		if (typeof json === 'string') { json = JSON.parse(json); }
+
+		var deferred = Q.defer();
+
+		if (!json.original_url) { deferred.reject(new Error('url not existent or not readable')); }
+
+		mongoose.models.EmbedlyExtractModel
+			.findOne({original_url: json.original_url})
+			.exec(function(err, embed) {
+				if (!embed) {
+					mongoose.models.EmbedlyExtractModel.create(json, function(err, embed) {
+						if (err || !embed) { deferred.reject(new Error(err)); return false; }
+						exports.assignEmbedToLink(embed, function(err, embed) {
+							if (err || !embed) { deferred.reject(new Error(err)); }
+							deferred.resolve(embed);
+						});
+					});
+				} else {
+					exports.assignEmbedToLink(embed, function(err, embed) {
+						if (err || !embed) { deferred.reject(new Error(err)); }
+						deferred.resolve(embed);
+					});
+				}
+			});
+
+		return deferred.promise;
+	};
+
+	exports.saveDocuments = function(json, callback) {
+		if (typeof json === 'string') { json = JSON.parse(json); }
+		if (!Array.isArray(json)) { json = [json]; }
+
+		var allOperationsAsPromises = [];
+		json.forEach(function(embedValue) {
+			var embedPromise = exports.promiseToSaveDocument(embedValue).fail(function(error) {
+				console.warn(error);
+			});
+			allOperationsAsPromises.push(embedPromise);
+		});
+
+		Q.allResolved(allOperationsAsPromises)
+			.then(function(promises) {
+				var embeds = [];
+				promises.forEach(function(promise) {
+					if (promise.isFulfilled()) {
+						embeds.push(promise.valueOf());
+					} else {
+						var exception = promise.valueOf().exception;
+					}
+				});
+				callback(embeds);
+			})
+			.fail(function(err) {
+				console.log(err);
+			});
+	};
+
 
 
 } ());
